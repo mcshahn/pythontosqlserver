d82008b2b72855040b20c37123f38a1cf66fb032
MichelleAhn <mcshahn@gmail.com>
2021-06-18 21:55:00 -0400

    Create dataframe_pop_row.py

---
+++ dataframe_pop_row.py
@@ -0,0 +1,15 @@
+#this function returns a dataframe after removing the row of a given index from a given dataframe
+import pandas as pd
+import numpy as np
+# Example dataframe for testing
+# df = pd.DataFrame({'col1': [1, 2, 3], 'col2': [3, 4, 5], 'col3': [5, 6, 7]})
+
+def dataframe_pop(df, idx = 0):
+    arr_size = df.shape[0]
+    arr  = np.ones(arr_size, dtype=bool)
+
+    arr[idx] = False
+    return df[arr]
+
+# df.shape
+print(dataframe_pop(df, 2))
